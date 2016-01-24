fdef11e702dbbe2620367a57736d9afc76996fa5
pitancheff <m_parvanoff@mail.bg>
2016-01-24 22:31:31 +0200

    Adding a basic MVC projecrt

---
+++ MVC_WebApp/MVC_WebApp/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using MVC_WebApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_WebApp.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        //public ActionResult Index()
+        //{
+        //    return View();
+        //}
+
+        public string MyFirstMethod()
+        {
+            return "Welcome to QuestPond!!!";
+        }
+
+        public ActionResult MyFirstViewMethod()
+        {
+
+            CustomerBAL b = new CustomerBAL();
+            Customer c = b.GetCustomer();
+            ViewBag.CustomerData = c;
+
+            //string CustomerName = "Mr. ABC";
+            //ViewData["MyData"] = CustomerName;
+            ////ViewBag.MyData = CustomerName;
+            return View("MyFirstView");
+        }
+
+    }
+}
---
+++ MVC_WebApp/MVC_WebApp/Models/CustomerBAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_WebApp.Models
+{
+    public class CustomerBAL
+    {
+        public Customer GetCustomer()
+        {
+            Customer c = new Customer();
+            c.CustomerName = "Some Customer";
+            c.Address = "Some Address";
+            c.Age = 55;
+            return c;
+        }
+    }
+}
